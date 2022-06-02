df38dae1dee642a3ae253ea51386d6299d3bf751
DigdoAji <1910817310001@mhs.ulm.ac.id>
2022-06-02 23:15:31 +0800

    Update file/folder pada proyek

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,6 @@ import '../styles/main.css';
 import 'regenerator-runtime';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import main from './util/main';
+
+document.addEventListener('DOMContentLoaded', main);
