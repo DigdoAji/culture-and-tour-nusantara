aa470276bffbad1eeaea28edc26cd558230f35c5
DigdoAji <1910817310001@mhs.ulm.ac.id>
2022-06-03 22:12:40 +0800

    Mengembalikan file dan folder

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import '../styles/main.css';
+import 'regenerator-runtime';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import main from './util/main';
+
+document.addEventListener('DOMContentLoaded', main);
